706fa421ffd60955c1e4b7cae4ec980bea07ef14
IndiraP979 <indirap.pd@gmail.com>
2025-08-24 16:57:55 +0530

    09Aug

--- src/test/java/com/thetestingacademy/ex01_Selenium_Basics/TestSelenium01.java
+++ src/test/java/com/thetestingacademy/ex01_Selenium_Basics/TestSelenium01.java
@@ -1,4 +1,26 @@
 package com.thetestingacademy.ex01_Selenium_Basics;
 
-public class TestSelenium01 {
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestSelenium01
+
+{
+
+    @Test
+    public void test_Code()
+    {
+        Assert.assertEquals("indira","Indira");
+    }
+
+    @Test
+    public void test_OpenVWOLoginPage()
+    {
+        FirefoxDriver driver=new FirefoxDriver();
+        driver.get("https://app.vwo.com");
+
+    }
 }
